caf511bd2f9c2a017d3ed2f349d9adfe1bc43ed2
Tobio1 <1932765+Tobio1@users.noreply.github.com>
2020-04-17 21:21:41 +0200

    added mcnemar

--- Dataloader.R
+++ Dataloader.R
@@ -2,27 +2,37 @@
 
 
 # Load libraries --------
+library(readxl)
 library(DTComPair) #Library for diagnostic comparison
 
 # Import ------------------------------------------------------------------
 #Import file manually using standard import function of Rstudio
-data<-X200125_LAM_Merged_data_PP_MSD_MSDconc_Cohorts1_RITJATA_Compared2AZ_HIVall
+r<- read_excel("~/Google Drive/13_Publications/190600_FujiLAM_HIVneg/DATA/200125_LAM_Merged_data_PP_MSD_MSDconc_Cohorts1_RITJATA_Compared2AZ_HIVall.xlsx", 
+                                                                                         na = "NaN")
+rd <- subset(r, r$AHIVneg==1)
 
 
 # locate and standardize diagnostic data based on column headers --------------------------
-mrs<-data$MRS
-crs<-data$CRS
-cd4<-data$CD4CNT
-index<-data$FujiRIT_FC
-comp1<-data$AlereRIT_FC
+upid<-rd$UPID...1
+mrs<-rd$MRS           #main reference standard
+crs<-rd$CRS           #alternative reference standard
+index<-rd$FujiRIT_FC  #index test for main analysis
+comp1<-rd$AlereRIT_FC #comparator test for main analysis
+sex<-rd$SEX 
+age<-rd$AGE
+cd4<-rd$CD4CNT
+sp1xp<-rd$SP1_XP
+sp2xp<-rd$SP2_XP
+sp3xp<-rd$SP3_XP
+spxpany<-rd$SP_XP_ANY
 
-d = data.frame(mrs, crs, cd4, index, comp1)
+d = data.frame(upid,mrs, crs, cd4, index, comp1, sex, age, sp1xp)
 
 
-# Diagnostic accuracy -----------------------------------------------------
+# Compare Diagnostic accuracy -----------------------------------------------------
 tab<-tab.paired(d$mrs, d$index, d$comp1)
-acc<-acc.paired(tab)
-
+acc<-acc.paired(tab) 
+pval<-sesp.mcnemar(tab) #mcnemar p-values
 
 # Write results to .csv file ----------------------
 write.table("data/results.csv", output_file)
